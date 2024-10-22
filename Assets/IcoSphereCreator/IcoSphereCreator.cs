67b16ed957ce515264785e99ef4c78bc997c847f
dingsheng <1050092912@qq.com>
2024-10-22 19:22:11 +0800

    【工程研究】1. 用每个坐标轴上x,y,z的正负单位向量作为基准，先构建一个逻辑上的正八面体，2. 然后在此基础上可以细分成更多面的物体。3. 细分是通过向量的插值（非常不错的方式，要是我来根本就想不到）4. UV的计算，我也学习了。5. 可惜法线的计算，我不明白它的公式原理

--- Assets/IcoSphereCreator/IcoSphereCreator.cs
+++ Assets/IcoSphereCreator/IcoSphereCreator.cs
@@ -1,20 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.InteropServices;
 
 public static class IcoSphereCreator {
+	/*
+	level		vertex		triangle			ratio（最开始的一个三角形，被分割成小三角的数量）
+	1			24			8					x1
+	2			96	 		32					x4
+	3			216			72					x9
+	4 			384			128					x16
+	5			600			200					x25
+	6											x36
 
+	所以
+		1. level 表示一个三角形的一个边，被分割的线段数量
+		2. ratio 与 level 的公式： ratio = level ^ 2; 这个是根据公式推导出来的，不是根据规律推到的
+			一个三角形被分割成小的三角形，假设数量Sn, n就是level
+			an = n + (n - 1) = 2n - 1;(自己画画三角形，自己分割一下就能找到规律)
+			Sn = a1 + a2 + a3 + ... + an
+			这个时候就能把an拆解成 an = bn - 1; bn = 2n；(等比数列呼之欲出)
+			Xn = b1 + b2 + b3 + ... + bn = 2 * (1 + 2 + 3 + ... n) = n^2 + n;
+			Sn = Xn - n = n ^ 2;
 
-    public static Mesh Create(int n, float radius)
+	注意
+		1. 虽然逻辑上是对一个三角面片进行分割，但实际上，因为是向量的插值（直径不变），所以被分割的三角面片会 “隆起”
+		2. 构建出来的mesh, 其顶点数量并没有做优化，比如八面体，它有八个面，但是只需要6个顶点就可以了，但是构建的时候却用了24个顶点，因为相邻的三角形并没有共用顶点。
+			这样有坏处，也有好处。坏处就是内存占用较大；但是好处就是，构建出来的mesh是可以更具面片打散的，可以自定义出其他”奇幻“的效果。
+	*/
+
+    public static Mesh Create(int level, float radius)
     {
-        int nn = n * 4;
-        int vertexNum = (nn * nn / 16) * 24;
+        int nlevel = level * 4;
+        int vertexNum = (nlevel * nlevel / 16) * 24; // "nlevel * nlevel / 16" 其实就是一个面被分解成的小三角形的数量。按照上面的推到，可以被简化成 "level * level"
+
         Vector3[] vertices = new Vector3[vertexNum];
         int[] triangles = new int[vertexNum];
         Vector2[] uv = new Vector2[vertexNum];
+		Color[] colors = new Color[vertexNum];
 
         Quaternion[] init_vectors = new Quaternion[24];
         // 0
-        init_vectors[0] = new Quaternion(0, 1, 0, 0);   //the triangle vertical to (1,1,1)
+        init_vectors[0] = new Quaternion(0, 1, 0, 0);   //the triangle vertical to (1,1,1) 		
         init_vectors[1] = new Quaternion(0, 0, 1, 0);
         init_vectors[2] = new Quaternion(1, 0, 0, 0);
         // 1
@@ -59,28 +85,29 @@ public static class IcoSphereCreator {
 			 *  /________\   /________\/
 			 *     q->       a         b
 			 */
-            for (int p = 0; p < n; p++)
+            for (int p = 0; p < level; p++)
             {   
                 //edge index 1
-                Quaternion edge_p1 = Quaternion.Lerp(init_vectors[i], init_vectors[i + 2], (float)p / n);
-                Quaternion edge_p2 = Quaternion.Lerp(init_vectors[i + 1], init_vectors[i + 2], (float)p / n);
-                Quaternion edge_p3 = Quaternion.Lerp(init_vectors[i], init_vectors[i + 2], (float)(p + 1) / n);
-                Quaternion edge_p4 = Quaternion.Lerp(init_vectors[i + 1], init_vectors[i + 2], (float)(p + 1) / n);
+                Quaternion edge_p1 = Quaternion.Lerp(init_vectors[i], init_vectors[i + 2], (float)p / level);				// y -> x
+                Quaternion edge_p2 = Quaternion.Lerp(init_vectors[i + 1], init_vectors[i + 2], (float)p / level);			// z -> x
+                Quaternion edge_p3 = Quaternion.Lerp(init_vectors[i], init_vectors[i + 2], (float)(p + 1) / level);			// y -> x
+                Quaternion edge_p4 = Quaternion.Lerp(init_vectors[i + 1], init_vectors[i + 2], (float)(p + 1) / level);		// z -> x
 
-                for (int q = 0; q < (n - p); q++)
+                for (int q = 0; q < (level - p); q++)
                 {   
                     //edge index 2
-                    Quaternion a = Quaternion.Lerp(edge_p1, edge_p2, (float)q / (n - p));
-                    Quaternion b = Quaternion.Lerp(edge_p1, edge_p2, (float)(q + 1) / (n - p));
+                    Quaternion a = Quaternion.Lerp(edge_p1, edge_p2, (float)q / (level - p));
+                    Quaternion b = Quaternion.Lerp(edge_p1, edge_p2, (float)(q + 1) / (level - p));
                     Quaternion c, d;
+
                     if(edge_p3 == edge_p4)
                     {
                         c = edge_p3;
                         d = edge_p3;
                     }else
                     {
-                        c = Quaternion.Lerp(edge_p3, edge_p4, (float)q / (n - p - 1));
-                        d = Quaternion.Lerp(edge_p3, edge_p4, (float)(q + 1) / (n - p - 1));
+                        c = Quaternion.Lerp(edge_p3, edge_p4, (float)q / (level - p - 1));
+                        d = Quaternion.Lerp(edge_p3, edge_p4, (float)(q + 1) / (level - p - 1));
                     }
 
                     triangles[j] = j;
@@ -89,7 +116,8 @@ public static class IcoSphereCreator {
                     vertices[j++] = new Vector3(b.x, b.y, b.z);
                     triangles[j] = j;
                     vertices[j++] = new Vector3(c.x, c.y, c.z);
-                    if (q < n - p - 1)
+					
+                    if (q < level - p - 1)		// 什么情况下会有这步逻辑？答: 三角形是按照从下往上，从左往右被分割的；越往下，越往左（a,b,c,d是一个四边形，而不是三角形的时候），这步就会被调用
                     {
                         triangles[j] = j;
                         vertices[j++] = new Vector3(c.x, c.y, c.z);
@@ -104,7 +132,9 @@ public static class IcoSphereCreator {
         Mesh mesh = new Mesh();
         mesh.name = "IcoSphere";
 
-        CreateUV(n, vertices, uv);
+        CreateUV(level, vertices, uv);
+		CreateColor(level, vertices, colors);
+
         for (int i = 0; i < vertexNum; i++)
         {
             vertices[i] *= radius;
@@ -112,30 +142,31 @@ public static class IcoSphereCreator {
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.uv = uv;
+		mesh.colors = colors;
         mesh.RecalculateNormals();
         CreateTangents(mesh);
 
         return mesh;
     }
 
-    static void CreateUV(int n, Vector3[] vertices, Vector2[] uv)
+    static void CreateUV(int level, Vector3[] vertices, Vector2[] uv)
     {
-        int tri = n * n;        // devided triangle count (1,4,9...)
-        int uvLimit = tri * 6;  // range of wrap UV.x 
-        Debug.Log("tri " + tri + " uvLimit " + uvLimit);
+        int tri = level * level;        // devided triangle count (1,4,9...)
+        int uvLimit = tri * 6;  		// range of wrap UV.x 					它包含了第一和第二象限的所有顶点
 
         for (int i = 0; i < vertices.Length; i++)
         {
             Vector3 v = vertices[i];
 
             Vector2 textureCoordinates;
-            if((v.x == 0f)&&(i < uvLimit))
+            if((v.x == 0f)&&(i < uvLimit)) // 第一，第二象限，x == 0 的坐标，让它的 uv == 1 而不是 0；这样才能构成完成的逆时针。
             {
+				// 2
                 textureCoordinates.x = 1f;
             }
             else
             {
-                textureCoordinates.x = Mathf.Atan2(v.x, v.z) / (-2f * Mathf.PI);
+                textureCoordinates.x = Mathf.Atan2(v.x, v.z) / (-2f * Mathf.PI); //注意这里因为有一个-2,所以颠倒了。结合后面的那个x代码，所以uv按照 第二象限 -> 第三象限 -> 第四象限 -> 第一象限 顺序展开 
             }
 
             if (textureCoordinates.x < 0f)
@@ -143,13 +174,34 @@ public static class IcoSphereCreator {
                 textureCoordinates.x += 1f;
             }
 
-            textureCoordinates.y = Mathf.Asin(v.y) / Mathf.PI + 0.5f;
+            textureCoordinates.y = Mathf.Asin(v.y) / Mathf.PI + 0.5f; // uv按照从上往下展开
             uv[i] = textureCoordinates;
         }
 
-        int tt = tri * 3;
-        uv[0 * tt + 0].x = 0.875f;
-        uv[1 * tt + 0].x = 0.875f;
+// 		int tt = tri * 3;
+// 		Debug.Log($@"[0 * tt + 0]: {uv[0 * tt + 0].x} 
+// [1 * tt + 0]: {uv[1 * tt + 0].x} 
+// [2 * tt + 0]: {uv[2 * tt + 0].x} 
+// [3 * tt + 0]: {uv[3 * tt + 0].x} 
+// [4 * tt + 0]: {uv[4 * tt + 0].x} 
+// [5 * tt + 0]: {uv[5 * tt + 0].x} 
+// [6 * tt + 0]: {uv[6 * tt + 0].x} 
+// [7 * tt + 0]: {uv[7 * tt + 0].x}");
+		/*
+		[0 * tt + 0]: 1 
+		[1 * tt + 0]: 1 
+		[2 * tt + 0]: 0 
+		[3 * tt + 0]: 0 
+		[4 * tt + 0]: 0 
+		[5 * tt + 0]: 0 
+		[6 * tt + 0]: 0 
+		[7 * tt + 0]: 0
+		*/
+
+		// 1
+        int tt = tri * 3;			// 可以理解成是一个大面中的最后一个顶点，也就是越偏向X轴的点
+        uv[0 * tt + 0].x = 0.875f;	
+        uv[1 * tt + 0].x = 0.875f;	// 这里的索引表示，当前这个象限的第一个顶点。偏向Y轴两端的点
         uv[2 * tt + 0].x = 0.125f;
         uv[3 * tt + 0].x = 0.125f;
         uv[4 * tt + 0].x = 0.625f;
@@ -157,8 +209,42 @@ public static class IcoSphereCreator {
         uv[6 * tt + 0].x = 0.375f;
         uv[7 * tt + 0].x = 0.625f;
 
+		// 1_1
+		// 破案了，因为上下顶点，是很多个点浓缩成一个点(x = 0 , z = 0) 按照上面的公式，计算出来的u都是 == 0; 这是不合理，但是把上下8八点展开，重新手动赋值 uv 顶点
+		// int tt = tri * 3;
+        // uv[0 * tt + 0].x = 1f;
+        // uv[1 * tt + 0].x = 1f;
+        // uv[2 * tt + 0].x = 0f;
+        // uv[3 * tt + 0].x = 0f;
+        // uv[4 * tt + 0].x = 0.66f;
+        // uv[5 * tt + 0].x = 0.33f;
+        // uv[6 * tt + 0].x = 0.33f;
+        // uv[7 * tt + 0].x = 0.66f;
+
     }
 
+	// 4
+	static void CreateColor(int level, Vector3[] vertices, Color[] color)
+	{
+		for (int i = 0; i < color.Length; ++i)
+		{
+			color[i] = Color.black;
+		}
+
+		int tri = level * level;
+		int tt = tri * 3;			
+
+        color[0 * tt + 0] = Color.red;//第一象限，上顶点
+        color[1 * tt + 0] = Color.red;//第二象限，下顶点
+        color[2 * tt + 0] = Color.red;//第三象限，上顶点
+        color[3 * tt + 0] = Color.red;//第四象限，下顶点
+        color[4 * tt + 0] = Color.red;//第五象限，上顶点
+        color[5 * tt + 0] = Color.red;//第六象限，上顶点
+        color[6 * tt + 0] = Color.red;//第七象限，下顶点
+        color[7 * tt + 0] = Color.red;//第八象限，下顶点
+	}
+
+	// 5
     static void CreateTangents(Mesh mesh)
     {
         int[] triangles = mesh.triangles;
@@ -200,9 +286,9 @@ public static class IcoSphereCreator {
             float t1 = w2.y - w1.y;
             float t2 = w3.y - w1.y;
 
-            float r = 1.0f / (s1 * t2 - s2 * t1);
+            float r = 1.0f / (s1 * t2 - s2 * t1); // 这个是向量 叉乘 
 
-            Vector3 sdir = new Vector3((t2 * x1 - t1 * x2) * r, (t2 * y1 - t1 * y2) * r, (t2 * z1 - t1 * z2) * r);
+            Vector3 sdir = new Vector3((t2 * x1 - t1 * x2) * r, (t2 * y1 - t1 * y2) * r, (t2 * z1 - t1 * z2) * r);// 这个公式，都看不懂
             Vector3 tdir = new Vector3((s1 * x2 - s2 * x1) * r, (s1 * y2 - s2 * y1) * r, (s1 * z2 - s2 * z1) * r);
 
             tan1[i1] += sdir;
@@ -230,4 +316,5 @@ public static class IcoSphereCreator {
 
         mesh.tangents = tangents;
     }
+
 }
---
+++ Assets/Scripts/TestAtan.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAtan : MonoBehaviour
+{
+	public Transform target;
+
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			Vector3 relative = transform.InverseTransformPoint(target.position);
+        	float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
+			float angle2 = Mathf.Atan(relative.x / relative.z) * Mathf.Rad2Deg;
+			print($"angle: {angle}; angle2: {angle2}; color: {new Color()}");
+        	// transform.Rotate(0, angle, 0);
+		}
+    }
+
+	[ContextMenu("TestTangle")]
+	void TestTangle()
+	{
+		Vector3 normal = new Vector3(0.0f, 1.0f, 1.0f);  // 法线
+		Vector3 tangent = new Vector3(1.0f, 0.0f, 0.0f); // 切线
+
+		Vector3.OrthoNormalize(ref normal, ref tangent);
+
+		print($"[TestTangle]. normal: {normal}; tangent: {tangent}");
+	}
+}
